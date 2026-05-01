1968b39fb389e9fade9e7e98a010ef5ae85e4425
hoa <23120127@student.hcmus.edu.vn>
2026-05-01 17:19:12 +0700

    test: add safe code to show sonarcloud passing

--- customer/src/main/java/com/yas/customer/CustomerApplication.java
+++ customer/src/main/java/com/yas/customer/CustomerApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class CustomerApplication {
 
     public static void main(String[] args) {
+        String safePassword = System.getenv("DB_PASSWORD"); // SECURE: using env var
+        System.out.println("Connecting with secure password retrieval logic.");
         SpringApplication.run(CustomerApplication.class, args);
     }
 }
